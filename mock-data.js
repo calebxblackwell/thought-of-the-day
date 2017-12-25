2217b6ece694183a4cedd0fca63e98b469ace415
Caleb Blackwell <calebxblackwell@gmail.com>
2017-12-24 19:53:24 -0800

    added mock-data.js

---
+++ mock-data.js
@@ -0,0 +1,33 @@
+//mock data for the api
+var mockData = {
+  "textEntered": [
+    {
+      "date": "12/24",
+      "text": "Merry Christmas Eve.",
+      "publishedAt":
+    },
+    {
+      "date":"12/25",
+      "text": "Merry Christmas!",
+      "publishedAt":
+    }
+  ]
+};
+//this is the function that uses AJAX to call to the server
+function getData(callbackFn){
+  setTimeout(function(){callbackFn(mockData)},1);
+}
+//these functions stay the same after we have the real API
+function displayData(data){
+  for (index in data.mockData){
+    $('body').append(
+      '<p>' + data.mockData[index].text + '</p>');
+  }
+}
+function getAndDisplayData(){
+  getData(displayData);
+}
+//on page load:
+$(function(){
+  getAndDisplayData();
+});
