e9dfaa49399dc6157469a4849d61f2e3a6ab91ae
Caleb Blackwell <calebxblackwell@gmail.com>
2017-12-28 12:49:28 -0800

    added endpoints

--- test-server.js
+++ test-server.js
@@ -17,3 +17,68 @@ describe('index page', function () {
       });
   });
 });
+describe('API endpoints', function(){
+  before(function() {
+    return runServer();
+  });
+  beforeEach(function(){
+    return seedMockData();
+  });
+  after(function() {
+    return closeServer();
+  });
+describe('access root', function(){
+  it('should return 200 and html', function(){
+    return chai.request(app)
+    .get('/')
+    .then(function(res){
+      res.should.have.status(200);
+      res.should.be.html;
+    });
+  });
+});
+describe('POST endpoint', function(){
+  it('should add a new status', function(){
+    const textEntered=STATUS;
+    return chai.request(app)
+    .post('/status')
+    .send(textEntered)
+    .then(function(res){
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.include.keys('status','returns');
+      res.body.status.should.equal(textEntered.status);
+      return status.findbyDate(res.body.date);
+    });
+    .then(function(status){
+      textEntered.should.equal(status.date);
+    });
+  });
+});
+  describe('PUT endpoint', function(){
+    it('should post status and attach date', function(){
+      const findbyDate = {
+        returns: 'date of post'
+      };
+        return textEntered
+        .then(show => {
+          displayData.data = textEntered;
+
+          return chai.request(app)
+          .put (`/textEntered/${textEntered}`)
+          .send(displayData);
+        });
+        .then(res => {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+
+          return textEntered.getData(res.body).exec();
+        });
+        .then(post => {
+          post.should.equal(displayData.getData);
+        });
+    });
+  });
+});
