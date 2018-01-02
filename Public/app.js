1526bebcbe8bfad2879cc037eb0d37bbd499c9d4
Caleb Blackwell <calebxblackwell@gmail.com>
2018-01-02 14:11:57 -0800

    tenth commit

--- Public/app.js
+++ Public/app.js
@@ -5,5 +5,19 @@ $(document).ready(function(){
           return response.json();
       });
   }
-  
+
 });
+//clicking add to post a STATUS
+//html button performing a POST request
+$('#AddButton').on('click', function(e){
+  $.post('.status', function(data, `/.status`){
+    alert("Data: " + data + '/.status: '+ status);
+  });
+  console.log("Add button clicked");
+  let textEntered = {
+    status: $('.status').val()
+  };
+  .then(res => {
+    displayData();
+  });
+}
