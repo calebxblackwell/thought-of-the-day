a6ccf93981c132db4b6c05bb410cca6f11f4f66b
Caleb Blackwell <calebxblackwell@gmail.com>
2018-01-25 14:46:43 -0800

    nineteenth commit

--- config.js
+++ config.js
@@ -6,3 +6,8 @@ exports.DATABASE_URL =
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'access_token';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+//test database url info?
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL ||
+  global.TEST_DATABASE_URL||
+  mongoose.connect('mongodb://localhost:27017/full-stack-capstone');
--- test-users.js
+++ test-users.js
@@ -1,4 +1,5 @@
 mongoose.connect('mongodb://localhost:27017/full-stack-capstone');
+const { TEST_DATABASE_URL } = require('../config');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 //in order to run user tests, you need to access the server.js and router.js info
@@ -8,12 +9,8 @@ const {User} = require('../router');//router.js file (has users info)
 describe('/router', function(){
   const username = 'testuser';
   const password = 'testpassword';
-  const firstName = 'Test';
-  const lastName = 'User';
   const usernameB = 'testuserB';
   const passwordB = 'testpasswordB';
-  const firstNameB = 'TestB';
-  const lastNameB = 'UserB';
 
 //this part of the test starts and stops the server.
 before(function() {
@@ -32,8 +29,6 @@ describe('/router', function() {
             .post('/router')
             .send({
               password,
-              firstName,
-              lastName
             })
             .then(() =>
               expect.fail(null, null, 'Request denied')
@@ -57,8 +52,6 @@ describe('/router', function() {
               .post('/router')
               .send({
                 username,
-                firstName,
-                lastName
               })
               .then(() =>
               expect.fail(null, null, 'Request denied')
@@ -83,8 +76,6 @@ describe('/router', function() {
      .send({
        username: 123456,
        password,
-       firstName,
-       lastName
      })
      .then(() =>
        expect.fail(null, null, 'Request denied')
@@ -111,8 +102,6 @@ it('Rejects users with non-string password', function() {
           .send({
             username,
             password: 123456,
-            firstName,
-            lastName
           })
           .then(() =>
             expect.fail(null, null, 'Request denied')
@@ -131,62 +120,6 @@ it('Rejects users with non-string password', function() {
             expect(res.body.location).to.equal('password');
           });
       });
-//this part rejects users with incorrect characters in first name
-it('Rejects users with non-string first name', function() {
-        return chai
-          .request(app)
-          .post('/router')
-          .send({
-            username,
-            password,
-            firstName: 123456,
-            lastName
-          })
-          .then(() =>
-            expect.fail(null, null, 'Request denied')
-          )
-          .catch(err => {
-            if (err instanceof chai.AssertionError) {
-              throw err;
-            }
-
-            const res = err.response;
-            expect(res).to.have.status(422);
-            expect(res.body.reason).to.equal('Not valid');
-            expect(res.body.message).to.equal(
-              'Incorrect field type: expected string'
-            );
-            expect(res.body.location).to.equal('firstName');
-          });
-      });
-//this part rejects users with incorrect characters in last name
-it('Rejects users with non-string last name', function() {
-        return chai
-          .request(app)
-          .post('/router')
-          .send({
-            username,
-            password,
-            firstName,
-            lastName: 123456
-          })
-          .then(() =>
-            expect.fail(null, null, 'Request denied')
-          )
-          .catch(err => {
-            if (err instanceof chai.AssertionError) {
-              throw err;
-            }
-
-            const res = err.response;
-            expect(res).to.have.status(422);
-            expect(res.body.reason).to.equal('Not valid');
-            expect(res.body.message).to.equal(
-              'Incorrect field type: expected string'
-            );
-            expect(res.body.location).to.equal('lastName');
-          });
-      });
 //this part rejects users with usernames with spaces
 it('Rejects users with non-trimmed username', function() {
         return chai
@@ -195,8 +128,6 @@ it('Rejects users with non-trimmed username', function() {
           .send({
             username: ` ${username} `,
             password,
-            firstName,
-            lastName
           })
           .then(() =>
             expect.fail(null, null, 'Request denied')
@@ -223,8 +154,6 @@ it('Rejects users with non-trimmed password', function() {
          .send({
            username,
            password: ` ${password} `,
-           firstName,
-           lastName
          })
          .then(() =>
            expect.fail(null, null, 'Request denied')
@@ -251,8 +180,6 @@ it('Rejects users with empty username field', function() {
           .send({
             username: '',
             password,
-            firstName,
-            lastName
           })
           .then(() =>
             expect.fail(null, null, 'Request denied')
@@ -279,8 +206,6 @@ it('Rejects users with password less than ten characters', function() {
          .send({
            username,
            password: '123456789',
-           firstName,
-           lastName
          })
          .then(() =>
            expect.fail(null, null, 'Request denied')
@@ -307,8 +232,6 @@ it('Rejects users with password greater than 72 characters', function() {
           .send({
             username,
             password: new Array(73).fill('a').join(''),
-            firstName,
-            lastName
           })
           .then(() =>
             expect.fail(null, null, 'Request denied')
@@ -333,16 +256,12 @@ it('Rejects users with duplicate username', function() {
    return User.create({
      username,
      password,
-     firstName,
-     lastName
    })
      .then(() =>
        // Try to create a second user with the same username
        chai.request(app).post('/router').send({
          username,
          password,
-         firstName,
-         lastName
        })
      )
      .then(() =>
@@ -370,68 +289,28 @@ it('Creates a new user', function() {
           .send({
             username,
             password,
-            firstName,
-            lastName
           })
           .then(res => {
             expect(res).to.have.status(201);
             expect(res.body).to.be.an('object');
             expect(res.body).to.have.keys(
               'username',
-              'firstName',
-              'lastName'
             );
             expect(res.body.username).to.equal(username);
-            expect(res.body.firstName).to.equal(firstName);
-            expect(res.body.lastName).to.equal(lastName);
             return User.findOne({
               username
             });
           })
           .then(user => {
             expect(user).to.not.be.null;
-            expect(user.firstName).to.equal(firstName);
-            expect(user.lastName).to.equal(lastName);
             return user.validatePassword(password);
           })
           .then(passwordIsCorrect => {
             expect(passwordIsCorrect).to.be.true;
           });
       });
-//this test creates first names and last names that don't have spaces.
-it('Trims firstName and lastName', function() {
-      return chai
-        .request(app)
-        .post('/router')
-        .send({
-          username,
-          password,
-          firstName: ` ${firstName} `,
-          lastName: ` ${lastName} `
-        })
-        .then(res => {
-          expect(res).to.have.status(201);
-          expect(res.body).to.be.an('object');
-          expect(res.body).to.have.keys(
-            'username',
-            'firstName',
-            'lastName'
-          );
-          expect(res.body.username).to.equal(username);
-          expect(res.body.firstName).to.equal(firstName);
-          expect(res.body.lastName).to.equal(lastName);
-          return User.findOne({
-            username
-          });
-        })
-        .then(user => {
-          expect(user).to.not.be.null;
-          expect(user.firstName).to.equal(firstName);
-          expect(user.lastName).to.equal(lastName);
-        });
-    });
-});
-//this tests the GET function.
+
+//this tests the GET function with the test user info
 describe('GET', function() {
   it('Returns an empty array initially', function() {
     return chai.request(app).get('/router').then(res => {
@@ -445,14 +324,10 @@ describe('GET', function() {
       {
         username,
         password,
-        firstName,
-        lastName
       },
       {
         username: usernameB,
         password: passwordB,
-        firstName: firstNameB,
-        lastName: lastNameB
       }
     )
       .then(() => chai.request(app).get('/router'))
@@ -462,13 +337,9 @@ describe('GET', function() {
         expect(res.body).to.have.length(2);
         expect(res.body[0]).to.deep.equal({
           username,
-          firstName,
-          lastName
         });
         expect(res.body[1]).to.deep.equal({
           username: usernameB,
-          firstName: firstNameB,
-          lastName: lastNameB
         });
       });
   });
